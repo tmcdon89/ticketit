2af17786735b6ea3e771c1223d793334eaa9e248
tmcdon89 <tmcdonald@Toms-MacBook-Pro.local>
2016-04-27 15:31:41 -0500

    Adjustments for installation

--- src/Views/install/index.blade.php
+++ src/Views/install/index.blade.php
@@ -76,7 +76,7 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
